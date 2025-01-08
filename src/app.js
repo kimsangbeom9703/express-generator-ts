f6d8ef11f171f4c7a4528e45495debb484b79805
kimsangbeom9703 <kp41589272@gmail.com>
2025-01-08 10:45:57 +0900

    dic change

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const createError = require('http-errors');
+const express = require('express');
+const cors = require('cors')
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+app.use(cors())
+
+const loggingLevel = process.env.LOGGING_LEVEL || 'dev'; // 기본값은 'dev'
+
+app.use(logger(loggingLevel));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
